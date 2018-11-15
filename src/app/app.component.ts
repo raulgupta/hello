2e597338c41c6b4942c90148020e3e9368ae164c
Rahul <rahul.gupta2608@yahoo.com>
2018-11-14 23:54:24 -0800

    creating first component in angular2

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public title = 'hello';
+  public email = 'rahul@synergistic.com';
+}
